5702754b04fd1f48d4e846ff2f1c13a3b6960a7e
JoaoTrombeta <joaotrombetadev@gmail.com>
2025-02-14 16:12:03 -0300

    Exercicio 9

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const prompt= require('prompt-sync')()
 
-let Exercicio = prompt("Digite o Exercício que deseja efetuar (digite o numero): \n 1 - média de 3 notas \n 2 - Celsius para Farnheit \n 3 - Farnheit para Celsius \n 4 - Cálculo de área \n 5 - Calcular àrea de Circulo \n \n")
+let Exercicio = prompt("Digite o Exercício que deseja efetuar (digite o numero): \n 1 - média de 3 notas \n 2 - Celsius para Farnheit \n 3 - Farnheit para Celsius \n 4 - Cálculo de área \n 5 - Calcular àrea de Circulo \n 6 - Calcular o perímetro de um Retângulo \n 7 - Calcular o perímetro de um Circulo \n 8 - Calcular numero elevado à X \n 9 - Converter Metros em Centímetros \n \n")
 if(Exercicio == "1"){
     let Num1 = prompt("\n \n Digite a nota do Primeiro Bimestre: ")
     let Num2 = prompt("\n \n Digite a nota do Segundo Bimestre: ")
@@ -26,4 +26,32 @@ if(Exercicio == "1"){
     let PI = 3.14159265359
     const Area = (R) => (parseInt(R) * parseInt(R)) * PI
     console.log("Area Circular: " + Math.round(Area(Raio)))
+}else if(Exercicio == "6"){
+    let Altura = prompt("\n \n Digite a altura do Retângulo: ")
+    let Largura = prompt("\n \n Digite a largura do Retângulo: ")
+    const Perimetro = (A,L) => (parseInt(A) * parseInt(A))+(parseInt(L) * parseInt(L))
+    console.log("Perímetro Retangular: " + Perimetro(Altura,Largura))
+}else if(Exercicio == "7"){
+    let Raio = prompt("\n \n Digite o Raio do Círculo: ")
+    let PI = 3.14159265359
+    const Perimetro = (R) => (parseInt(R) * 2) * PI
+    console.log("Area Circular: " + Math.round(Perimetro(Raio)))
+}else if(Exercicio == "8"){
+    let Base = prompt("\n \n Digite o Número Base: ")
+    let Expo = prompt("\n \n Digite o Número Expoente: ")
+
+    var Repeticoes = 1
+    var Resultado = Base
+    while(Repeticoes < Expo){
+        Resultado = Resultado * Base
+        Repeticoes++
+    }
+    console.log("Resultado: "+Resultado)
+    
+}else if(Exercicio == "9"){
+    let Metros = prompt("\n \n Digite quantos metros converter: ")
+
+    const Centimetros = (M) => M*100
+    console.log("Resultado: "+Centimetros(Metros))
+    
 }
